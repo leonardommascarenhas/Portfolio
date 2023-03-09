d48f3c27d3680dddf84c39c54203c886e2b9d92e
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-03-09 17:53:22 -0300

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Intro from "./components/Intro";
 
 const App: React.FC = () => {
   return (
-    <div className="h-full flex flex-col justify-center items-center bg-gray-700 text-center">
+    <div className="h-full flex flex-col items-center justify-center bg-gray-700 text-center">
       <Intro />
     </div>
   );
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,13 +3,14 @@ import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const Intro = () => {
   return (
-    <section className="font-roboto text-5xl text-white font-light">
-      <h1>
+    <section className=" font-roboto text-5xl text-white font-light space-y-3 transition-">
+      <h1 className="transition-all duration-500 translate-x-7">
         Oi, eu sou o <span className="text-blue-500">Leonardo Moura</span>.
       </h1>
-      <h2>
-        Eu sou criador de &lt;<span className="text-yellow-300">Experiências /</span>&gt;
-      </h2>
+      <h1>
+        Eu sou criador de &lt;<span className="text-yellow-300">Experiências</span> /&gt;
+      </h1>
+      <button>Cheque meus projetos</button>
     </section>
   );
 };
