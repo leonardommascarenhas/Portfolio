d2e94f6c6ba08741624c83fcc7ef65f8a2073bf9
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-03-13 14:42:35 -0300

    custom scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Intro from "./components/Intro";
 
 const App: React.FC = () => {
   return (
-    <div className="snap-y overflow-y-scroll w-screen h-screen bg-black opacity-95">
+    <div className="snap-y snap-proximity overflow-y-scroll w-screen h-screen bg-black opacity-95">
       <Intro />
       <Intro />
       <Intro />
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BsFillArrowDownCircleFill } from "react-icons/bs";
 
+//remove the classOnHover to "clear" the animation of the fadeIn button
 const removeClassOnHover = (classNameToRemove: string) => (e: React.MouseEvent<HTMLButtonElement>) => {
   e.currentTarget.classList.remove(classNameToRemove);
 };
@@ -18,7 +19,7 @@ const Intro = () => {
         <span className="text-yellow-300">Experiências</span> /&gt;
       </h1>
       <button
-        className="group inline-flex items-center gap-3 text-sm md:text-lg px-2 py-1 border border-blue-500 rounded-md animate-fadeIn fill-mode-forwards hover:-translate-y-0.5  hover:border-yellow-300 hover:transition-all"
+        className="group inline-flex items-center gap-3 text-sm md:text-lg px-2 py-1 border border-blue-500 rounded-md animate-fadeIn fill-mode-forwards hover:-translate-y-0.5  hover:border-yellow-300 hover:transition-all hover:shadow hover:shadow-yellow-400/50"
         onMouseEnter={handleHover}
       >
         Cheque meus projetos
