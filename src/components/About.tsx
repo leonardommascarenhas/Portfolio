d43b31269133e703024d48c6b606b1b34c7aa2a9
leonardommascarenhas <leonardommascarenhas@gmail.com>
2023-03-13 16:41:46 -0300

    add padding to About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <section className="snap-start h-screen text-center w-full">
-      <h2 className="text-2xl md:text-5xl pt-2">About</h2>
+      <h2 className="text-2xl md:text-5xl pt-5">About</h2>
     </section>
   );
 };
